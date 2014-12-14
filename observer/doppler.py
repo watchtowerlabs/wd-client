b3f0d5fea920ca4f4f5e62c3bcda877c71377894
icharitopoulos <icharitopoulos@gmail.com>
2014-12-14 12:17:04 +0200

    add EMPTY doppler function

---
+++ observer/doppler.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+def calculate_doppler_shift(frequency, distance=None, angular_velocity=None):
+    """ Calculates doppler effect frequency shift and returns new frequency.
+    """
+
+    #  THIS IS NOT IMPLEMENTED YET !!!
+
+    return frequency
