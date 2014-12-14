60fa67aa75a36131dc73b786f279ca8eaeecb383
icharitopoulos <icharitopoulos@gmail.com>
2014-12-14 12:36:23 +0200

    observer class properties

---
+++ observer/observer_config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+OBSERVER_ID = 'SATNOGS-1'
+OBSERVER_TOKEN = "abc"
+OBSERVER_LOCATION = {}
+
+# ROTCTL connection details
+ROT_IP = '127.0.0.1'
+ROT_PORT = 5005
+
+# RIGCTL connection details
+RIG_IP = '127.0.0.1'
+RIG_PORT = 6006
