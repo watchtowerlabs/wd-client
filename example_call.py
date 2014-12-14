696c69362d49c90133177755001b874dc97ffa6b
icharitopoulos <icharitopoulos@gmail.com>
2014-12-14 15:16:53 +0200

    observer skeleton

--- example_call.py
+++ example_call.py
@@ -3,7 +3,9 @@
 from observer import observer
 
 
-def start_new_observer():
+def start_new_observer(tle, observation_window, frequency):
     """ Initialises and starts a new observer and exits
     """
-    pass
+    new_observer = observer()
+    if new_observer.setup(tle, observation_window['end'], frequency):
+        new_observer.observe()
--- observer/observer.py
+++ observer/observer.py
@@ -5,6 +5,8 @@ class observer:
 
     _location = None
     _tle = None
+    _observation_end = None
+    _frequency = None
 
     _rot_ip = None
     _rot_port = None
@@ -14,7 +16,11 @@ class observer:
     _rig_port = None
     _rig_interval = None
 
+    #_start_timestamp = None  # it is requested that the observation starts now()
+    _end_timestamp = None
+
     def __init__(self):
+        #TODO: run setup in initialisation and throw exception in case of problem
         pass
 
     @property
@@ -64,3 +70,46 @@ class observer:
         if interval:
             self._rot_interval = interval
         return self._rot_interval
+
+    @property
+    def observation_end(self, timestamp):
+        if timestamp:
+            self._observation_end = timestamp
+        return self._observation_end
+
+    @property
+    def frequency(self, frequency):
+        if frequency:
+            self._frequency = frequency
+        return self._frequency
+
+    def setup(self, tle, observation_end, frequency):
+        """ Sets up required internal variables
+
+            returns True if setup is ok
+            returns False if setup had problems
+        """
+        setup_ok = True
+
+        setup_ok and (tle == self.tle(tle))
+        setup_ok and (observation_end == self.observation_end(observation_end))
+        setup_ok and (frequency == self.frequency(frequency))
+
+        return setup_ok
+
+    def observe(self):
+        """ starts threads for rotcrl and rigctl
+        """
+        # check for end timestamp
+
+        # start thread for rotctl
+        self.run_rot()
+
+        # start thread for rigctl
+        self.run_rig()
+
+    def run_rot(self):
+        pass
+
+    def run_rig(self):
+        pass
