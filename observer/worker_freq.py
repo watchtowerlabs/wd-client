1da5ee9f70433e4c68969240d6aa4e3cf9260c58
icharitopoulos <icharitopoulos@gmail.com>
2014-12-14 16:18:46 +0200

    addition of the frequency shift worker

--- observer/worker_freq.py
+++ observer/worker_freq.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-""" Class to facilitate tracking loop.
+""" Class to facilitate frequency shifting loop.
 """
 
 import trackersocket
@@ -8,13 +8,14 @@ import time
 import threading
 import json
 from datetime import datetime
+import doppler
 
 
-class WorkerTrack():
+class WorkerFreq():
 
     # socket to connect to
     _IP = '127.0.0.1'  # default is localhost
-    _PORT = 4533  # default receiver port
+    _PORT = 4534  # default receiver port
 
     # sleep time of loop
     SLEEP_TIME = 0.1  # in seconds  # TODO: get this from config?
@@ -27,16 +28,21 @@ class WorkerTrack():
     # end when this timestamp is reached
     _observation_end = None
 
+    # frequency of original signal
+    _frequency = None
+
     observer_dict = {}
     satellite_dict = {}
 
-    def __init__(self, ip=None, port=None, time_to_stop=None):
+    def __init__(self, ip=None, port=None, frequency=None, time_to_stop=None):
         """ We can pass receiver ip and port at initialisation.
         """
         if ip:
             self._IP = ip
         if port:
             self._PORT = port
+        if frequency:
+            self._frequency = frequency
         if time_to_stop:
             self._observation_end = time_to_stop
 
@@ -49,13 +55,8 @@ class WorkerTrack():
         """ Sets tracking object.
             Can also be called while tracking, to manipulate observation.
         """
-        #if self._debugmode:
-        #    print type(observer_dict), observer_dict
-        #    print type(satellite_dict), satellite_dict
         self.observer_dict = json.loads(observer_dict)
         self.satellite_dict = json.loads(satellite_dict)
-        #if self._debugmode:
-        #    print(('observer:', self.observer_dict, 'satellite:', self.satellite_dict))
 
     def trackstart(self):
         """ Starts the thread that communicates tracking info to remote socket.
@@ -73,7 +74,7 @@ class WorkerTrack():
 
             Uses observer and satellite objects set by trackobject().
 
-            Will exit if altitude is less than zero (below the horizon).
+            Will exit when observation_end timestamp is reached.
         """
         if self._debugmode:
             print(('alive:', self._stay_alive))
@@ -93,19 +94,18 @@ class WorkerTrack():
             else:
                 p = orbital.pinpoint(self.observer_dict, self.satellite_dict)
                 if p['ok']:
-                    if p['alt'] < 0:
-                        # break
-                        self._stay_alive = False
-                    else:
-                        s = 'P ' + str((p['az'].conjugate())) + ' ' + str(p['alt'].conjugate())
-                        #print s + str('\n')
-                        sock.send(s + str('\n'))
-                        time.sleep(self.SLEEP_TIME)
+                    shift = self.calculate_frequency_shift(self._frequency, p['rng'], p['rng_vlct'])
+                    s = str(shift) #TODO: FORMAT THIS AS REQUIRED
+                    sock.send(s + str('\n'))
+                    time.sleep(self.SLEEP_TIME)
             # exiting
         if self._debugmode:
-            print('Tracking thread exited.')
+            print('Frequency shifting thread exited.')
         else:
             sock.disconnect()
+
+    def calculate_frequency_shift(frequency, distance, angular_velocity):
+        return doppler(frequency, distance, angular_velocity)
 
     def trackstop(self):
         """ Sets object flag to false and stops the tracking thread.
