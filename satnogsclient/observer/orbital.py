53d37eb67a78316f821c6b5c3e9478e4dca85795
jinxcat <icharitopoulos@gmail.com>
2015-03-11 22:15:36 +0100

    Refactor orbital into class #13

--- satnogsclient/observer/orbital.py
+++ satnogsclient/observer/orbital.py
@@ -1,56 +1,106 @@
 # -*- coding: utf-8 -*-
-import sys
-
 from datetime import datetime
-
-import ephem
 import pytz
+import ephem
 
 
-def pinpoint(observer_dict, satellite_dict, timestamp=None):
+class Orbital:
     """
-    Provides azimuth and altitude of tracked object.
-
-    args:
-        observer_dict: dictionary with details of observation point.
-        satellite_dict: dictionary with details of satellite.
-        time: timestamp we want to use for pinpointing the observed object.
-
-        returns:
-            Dictionary containing azimuth, altitude and "ok" for error detection.
+    Provides calculations for orbital tracking. Wraps around pyephem.
     """
-    # observer object
-    if all(map(lambda x: x in observer_dict, ['lat', 'lon', 'elev'])):
-        observer = ephem.Observer()
-        observer.lon = str(observer_dict['lon'])
-        observer.lat = str(observer_dict['lat'])
-        observer.elevation = float(observer_dict['elev'])
-    else:
-        return {'ok': False}
+    observer = None
+    tle = None
 
-    # satellite object
-    if all(map(lambda x: x in satellite_dict, ['tle0', 'tle1', 'tle2'])):
-        tle0 = str(satellite_dict['tle0'])
-        tle1 = str(satellite_dict['tle1'])
-        tle2 = str(satellite_dict['tle2'])
-        try:
-            satellite = ephem.readtle(tle0, tle1, tle2)
-        except ValueError:
-            print(('error:', 'ephem object', 'tle values', sys.exc_info()[0]))
-            return {'ok': False}
-        except:
-            print(('error:', 'ephem object', sys.exc_info()[0]))
-            return {'ok': False}
-    else:
-        return {'ok': False}
+    def __init__(self, observer_dict=None, satellite_dict=None):
+        # observer object
+        if observer_dict:
+            ret = self._set_observer_dict(observer_dict)
+            if not ret:
+                return False
 
-    # time of observation
-    if not timestamp:
-        timestamp = datetime.now(pytz.utc)
+        # satellite object
+        if satellite_dict:
+            ret = self._set_satellite_dict(satellite_dict)
+            if not ret:
+                return False
 
-    # observation calculation
-    observer.date = timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')
-    satellite.compute(observer)
-    return {'alt': satellite.alt, 'az': satellite.az,
-            'rng': satellite.range, 'rng_vlct': satellite.range_velocity,
-            'ok': True}
+        # no problems
+        return True
+
+    def _set_observer(self, observer_dict):
+        """
+        Uses the dictionary provided to set observer attributes used in pyephem calculations.
+        """
+        if observer_dict:
+            if all([x in observer_dict for x in ['lat', 'lon', 'elev']]):
+                self.observer = ephem.Observer()
+                self.observer.lon = str(observer_dict['lon'])
+                self.observer.lat = str(observer_dict['lat'])
+                self.observer.elevation = float(observer_dict['elev'])
+                return True
+        return False
+
+    def _set_satellite(self, satellite_dict):
+        """
+        Uses the dictionary provided to set satellite trajectory attributes used in pyephem calculations.
+        """
+        if satellite_dict:
+            if all([x in satellite_dict for x in ['tle0', 'tle1', 'tle2']]):
+                tle0 = str(satellite_dict['tle0'])
+                tle1 = str(satellite_dict['tle1'])
+                tle2 = str(satellite_dict['tle2'])
+                try:
+                    self.satellite = ephem.readtle(tle0, tle1, tle2)
+                    return True
+                except ValueError:
+                    #return {'ok': False,, 'error': 'ephem object - tle values problematic: {}'.format(sys.exc_info()[0])}
+                    return False
+                except:
+                    #return {'ok': False,, 'error': 'ephem object: {}'.format(sys.exc_info()[0])}
+                    return False
+        return False
+
+    def pinpoint(self, observer_dict=None, satellite_dict=None, timestamp=None):
+        """
+        Provides azimuth and altitude of tracked object.
+
+        args:
+            observer_dict: dictionary with details of observation point.
+            satellite_dict: dictionary with details of satellite.
+            time: timestamp we want to use for pinpointing the observed object.
+
+            returns:
+                Dictionary containing azimuth, altitude and "ok" for error detection.
+                If errors are present, 'error' is included with error reason.
+        """
+        # check and set observer object, if dict provided
+        if observer_dict:
+            ret = self._set_observer_dict(observer_dict)
+            if not ret:
+                print(())
+                return {'ok': False, 'error': 'problem setting observer provided'}
+
+        # check and set satellite object, if dict provided
+        if satellite_dict:
+            ret = self._set_satellite_dict(satellite_dict)
+            if not ret:
+                return {'ok': False, 'error': 'problem setting satellite provided'}
+
+        # check if observer is set
+        if not self.observer:
+            return {'ok': False, 'error': 'observer not set'}
+
+        # check if satellite is set
+        if not self.satellite:
+            return {'ok': False, 'error': 'satellite not set'}
+
+        # time of observation
+        if not timestamp:
+            timestamp = datetime.now(pytz.utc)
+        self.observer.date = timestamp.strftime('%Y-%m-%d %H:%M:%S.%f')
+
+        # observation calculation
+        self.satellite.compute(self.observer)
+        return {'alt': self.satellite.alt, 'az': self.satellite.az,
+                'rng': self.satellite.range, 'rng_vlct': self.satellite.range_velocity,
+                'ok': True}
