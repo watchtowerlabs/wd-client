edfb15cdb021c472bda788524044ae9d2e7e8884
Alexander Schultze <alexschultze@gmail.com>
2015-03-24 21:46:23 +0100

    Multiple fixes *change timeformat for a better file name (":" not valid in windows filenames) *set default jobid to 1, because 0 fails "is not" test *remove error when elevation does not exist (0 is a valid value) *include gitignore to for .idea directories *https: catch https certificicate error, retry without validation *disable SSL certificate verification *default path for sqlite and data now home directory, create dir of not exist

--- satnogsclient/settings.py
+++ satnogsclient/settings.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+import os
 from os import environ, path
 
-PWD = path.dirname(path.realpath(__file__))
-DEFAULT_SQLITE_PATH = path.join(PWD, 'jobs.sqlite')
+
 
 
 def _cast_or_none(func, value):
@@ -16,11 +16,18 @@ GROUND_STATION_LAT = _cast_or_none(float, environ.get('SATNOGS_STATION_LAT', Non
 GROUND_STATION_LON = _cast_or_none(float, environ.get('SATNOGS_STATION_LON', None))
 GROUND_STATION_ELEV = _cast_or_none(float, environ.get('SATNOGS_STATION_ELEV', None))
 
+# PWD = path.dirname(path.realpath(__file__))
+OUTPUT_PATH = environ.get('SATNOGS_OUTPUT_PATH', '/tmp/.satnogs')
+if not os.path.exists(OUTPUT_PATH ):
+        os.makedirs(OUTPUT_PATH )
+
+
+DEFAULT_SQLITE_PATH = path.join(OUTPUT_PATH, 'jobs.sqlite')
 SQLITE_URL = environ.get('SATNOGS_SQLITE_URL', 'sqlite:///' + DEFAULT_SQLITE_PATH)
 DEMODULATION_COMMAND = environ.get('SATNOGS_DEMODULATION_COMMAND', 'rtl_fm')
 ENCODING_COMMAND = environ.get('SATNOGS_ENCODING_COMMAND', 'oggenc')
 DECODING_COMMAND = environ.get('SATNOGS_DECODING_COMMAND', 'multimon-ng')
-OUTPUT_PATH = environ.get('SATNOGS_OUTPUT_PATH', '/tmp')
+
 NETWORK_API_URL = environ.get('SATNOGS_API_URL', 'https://dev.satnogs.org/api/')
 NETWORK_API_QUERY_INTERVAL = 5  # In minutes
 SCHEDULER_SLEEP_TIME = 10  # In seconds
