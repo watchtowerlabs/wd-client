96224861798eeb55f31ace9878ff713de3856009
Alexander Schultze <alexschultze@gmail.com>
2015-04-15 22:39:18 +0200

    settings.py: include CA_CERT variable

--- satnogsclient/settings.py
+++ satnogsclient/settings.py
@@ -3,9 +3,6 @@ import os
 from os import environ, path
 from decouple import config
 
-
-
-
 def _cast_or_none(func, value):
     try:
         return func(value)
@@ -14,10 +11,10 @@ def _cast_or_none(func, value):
 
 
 # Read from settings.ini using python-decouple
-GROUND_STATION_ID = config('STATION_ID',cast=int, default=0)
-GROUND_STATION_LAT = config('STATION_LAT',cast=float, default=0)
-GROUND_STATION_LON = config('STATION_LON',cast=float, default=0)
-GROUND_STATION_ELEV = config('STATION_ELEV',cast=float, default=0)
+GROUND_STATION_ID = config('STATION_ID', cast=int, default=0)
+GROUND_STATION_LAT = config('STATION_LAT', cast=float, default=0)
+GROUND_STATION_LON = config('STATION_LON', cast=float, default=0)
+GROUND_STATION_ELEV = config('STATION_ELEV', cast=float, default=0)
 
 ROT_IP = config('ROT_IP', default='127.0.0.1')
 ROT_PORT = config('ROT_PORT', cast=int, default=4532)
@@ -26,9 +23,11 @@ RIG_PORT = config('RTL_TCP_PORT', cast=int, default=4532)
 
 NETWORK_API_URL = config('NETWORK_API_URL', default='https://dev.satnogs.org/api/')
 NETWORK_API_KEY = config('NETWORK_API_KEY', default='')
-NETWORK_API_QUERY_INTERVAL = config('NETWORK_API_QUERY_INTERVAL', cast=int, default=5) #minutes
-SCHEDULER_SLEEP_TIME = config('SCHEDULER_SLEEP_TIME', cast=int, default=5) #seconds
-
+NETWORK_API_QUERY_INTERVAL = config('NETWORK_API_QUERY_INTERVAL', cast=int, default=5)
+# minutes
+SCHEDULER_SLEEP_TIME = config('SCHEDULER_SLEEP_TIME', cast=int, default=5)
+# seconds
+CA_CERT = config('CA_CERT', default='')
 
 
 WORKINGDIR = config('WORKINGDIR', '/tmp/.satnogs')
