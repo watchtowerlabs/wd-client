d681bd8d7cd39013a9a5664f2d2366eac544227b
Kostis Triantafyllakis <ctriant@csd.uoc.gr>
2016-03-29 19:12:35 +0300

    Add post request functionality

--- satnogsclient/web/static/js/satnogs.js
+++ satnogsclient/web/static/js/satnogs.js
@@ -7,6 +7,24 @@ $(function(){
 	    array.configuration[row.cells[0].innerText] = row.cells[1].innerText;	    
 	}
 	var json = JSON.stringify(array);
+	postJSONData(json, "POST", "/config_update");
 	console.log(json);
   })
 });
+
+function postJSONData(JSONData, localMode, url) {
+        var localJSONData = JSONData;
+        var postMode = localMode;
+
+         $.ajax({
+                type: postMode,
+                url: url,
+                contentType:"application/json; charset=utf-8",
+                dataType:"json"
+                data:  JSONData
+                success: function(data){
+					console.log("POST success");
+                }   // Success Function
+		}); // AJAX Call
+
+}
