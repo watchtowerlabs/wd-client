ee8e45dfae5a945b2eea99d5418dac32c216ee35
George Vardakis <vardakis@csd.uoc.gr>
2016-04-05 12:14:37 +0300

    Added two modules for handling json and udp sockets

---
+++ satnogsclient/observer/udpsocket.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import logging
+import socket
+
+
+logger = logging.getLogger('satnogsclient')
+
+
+class Udpsocket:
+    """
+    Handles connectivity with remote ctl demons
+    Namely: rotctl and rigctl
+    
+    """
+
+    _BUFFER_SIZE = 2048
+    _connected = False
+
+    def __init__(self, ip, port):
+        self._UDP_IP = ip
+        self._UDP_PORT = port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    @property
+    def ip(self):
+        return self._UDP_IP
+
+    @ip.setter
+    def ip(self, new_ip):
+        self._UDP_IP = new_ip
+
+    @property
+    def port(self):
+        return self._UDP_PORT
+
+    @port.setter
+    def port(self, new_port):
+        self._UDP_PORT = new_port
+
+    @property
+    def buffer_size(self):
+        return self._BUFFER_SIZE
+
+    @buffer_size.setter
+    def buffer_size(self, new_buffer_size):
+        self._BUFFER_SIZE = new_buffer_size
+
+    @property
+    def is_connected(self):
+        return self._connected
+
+    def connect(self):
+        try:
+            logger.debug('Opening TCP socket: {0}:{1}'.format(self.ip, self.port))
+            self.s.connect((self.ip, self.port))
+            self._connected = True
+        except:
+            logger.error('Cannot connect to socket {0}:{1}'.format(self.ip, self.port))
+            self._connected = False
+        return self.is_connected
+
+    def send(self, message):
+        #if not self.is_connected:
+        #    self.connect()
+        logger.debug('Sending message: {0}'.format(message))
+        self.s.sendto(message,(self._UDP_IP , self._UDP_PORT))
+        #response = self.s.recv(self._BUFFER_SIZE)
+        #logger.debug('Received message: {0}'.format(response))
+
+    def disconnect(self):
+        logger.info('Closing socket: {0}'.format(self.s))
+        self.s.close()
+        self._connected = False
