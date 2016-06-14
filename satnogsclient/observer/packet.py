53e424649dd70ef445517cb05990b8f2ac899c80
George Vardakis <vardakis@csd.uoc.gr>
2016-06-14 20:25:48 +0300

    Mostly architecture changes

---
+++ satnogsclient/observer/packet.py
@@ -0,0 +1,194 @@
+import logging
+import socket
+import binascii
+import struct
+import ctypes
+
+from satnogsclient import packet_settings
+from satnogsclient.observer.commsocket import Commsocket
+from satnogsclient.observer.udpsocket import Udpsocket
+
+logger = logging.getLogger('satnogsclient')
+
+
+                
+def ecss_encoder(port):
+    logger.info('Started ecss encoder')
+    sock = Commsocket('127.0.0.1',port)
+    sock.bind()
+    sock.listen()
+    while 1:
+        conn = sock.accept()
+        if conn:
+            data = conn.recv(sock.tasks_buffer_size)
+            ecss_packetizer(data)
+                
+def ecss_depacketizer(buf):
+    size = len(buf)
+    assert((buf != NULL) == true)
+    assert((size > packet_settings.MIN_PKT_SIZE and size < packet_settings.MAX_PKT_SIZE) == true)
+    tmp_crc1 = buf[size - 1];
+    for i in range(0,size -2):
+        tmp_src2 = tmp_src2 ^ buf[i]
+
+    ver = buf[0] >> 5;
+
+    pkt_type = (buf[0] >> 4) & 0x01;
+    dfield_hdr = (buf[0] >> 3) & 0x01;
+
+    pkt_app_id = buf[1];
+
+    pkt_seq_flags = buf[2] >> 6;
+    t = bytearray(2)
+    t[0] = buf[2]
+    t[1] = buf[3]
+    t.reverse()
+    pkt_seq_count = t & 0x3FFF;
+    
+    t = bytearray(2)
+    t[0] = buf[4]
+    t[1] = buf[5]
+    t.reverse()
+
+    pkt_len = t
+
+    ccsds_sec_hdr = buf[6] >> 7;
+
+    tc_pus = buf[6] >> 4;
+
+    pkt_ack = 0x07 & buf[6];
+
+    pkt_ser_type = buf[7];
+    pkt_ser_subtype = buf[8];
+    pkt_dest_id = buf[9];
+
+    pkt_verification_state = packet_settings.SATR_PKT_INIT
+
+    if not ((pkt_app_id < packet_settings.LAST_APP_ID) == true) :
+        pkt_verification_state = packet_settings.SATR_PKT_ILLEGAL_APPID
+        return packet_settings.SATR_PKT_ILLEGAL_APPID; 
+
+    if not ((pkt_len == size - packet_settings.ECSS_HEADER_SIZE - 1) == true):
+        pkt_verification_state = packet_settings.SATR_PKT_INV_LEN
+        return packet_settings.SATR_PKT_INV_LEN; 
+    pkt_len = pkt_len - packet_settings.ECSS_DATA_HEADER_SIZE - packet_settings.ECSS_CRC_SIZE + 1;
+
+    if not ((tmp_crc1 == tmp_crc2) == true) :
+        pkt_verification_state = packet_settings.SATR_PKT_INC_CRC
+        return packet_settings.SATR_PKT_INC_CRC; 
+
+    if not((packet_settings.SERVICES_VERIFICATION_TC_TM[pkt_ser_type][pkt_ser_subtype][pkt_type] == 1) == true) : 
+        pkt_verification_state = packet_settings.SATR_PKT_ILLEGAL_PKT_TP
+        return packet_settings.SATR_PKT_ILLEGAL_PKT_TP; 
+
+    if not ((ver == packet_settings.ECSS_VER_NUMBER) == true) :
+        pkt_verification_state = packet_settings.SATR_ERROR
+        return packet_settings.SATR_ERROR; 
+
+    if not ((tc_pus == ECSS_PUS_VER) == true) :
+        pkt_verification_state = packet_settings.SATR_ERROR
+        return packet_settings.SATR_ERROR;
+
+    if not ((ccsds_sec_hdr == packet_settings.ECSS_SEC_HDR_FIELD_FLG) == true) :
+        pkt_verification_state = packet_settings.SATR_ERROR
+        return packet_settings.SATR_ERROR;
+
+    if not ((pkt_type == 'TC' or pkt_type == 'TM') == true) :
+        pkt_verification_state = packet_settings.SATR_ERROR
+        return packet_settings.SATR_ERROR;
+
+    if not ((dfield_hdr == packet_settings.ECSS_DATA_FIELD_HDR_FLG) == true) :
+        pkt_verification_state = packet_settings.SATR_ERROR
+        return packet_settings.SATR_ERROR;
+
+    if not ((pkt_ack == packet_settings.TC_ACK_NO or pkt_ack == packet_settings.TC_ACK_ACC) == true) :
+        pkt_verification_state = packet_settings.SATR_ERROR
+        return packet_settings.SATR_ERROR;
+
+    if not ((pkt_seq_flags == packet_settings.TC_TM_SEQ_SPACKET) == true) :
+        pkt_verification_state = packet_settings.SATR_ERROR
+        return packet_settings.SATR_ERROR; 
+    pkt_data = bytearray(pkt_len)
+
+    pktdata = buf[packet_settings.ECSS_DATA_OFFSET : size -2]
+    
+    return packet_settings.SATR_OK;
+                
+def ecss_decoder(port):
+    logger.info('Started ecss decoder')
+    sock = Commsocket('127.0.0.1',port)
+    sock.bind()
+    sock.listen()
+    while 1:
+        conn = sock.accept()
+        if conn:
+            data = conn.recv(sock.tasks_buffer_size)
+            ecss_depacketizer(data)
+         
+         
+            
+def ecss_packetizer(ecss):
+    sock = Commsocket(packet_settings.FRAME_RECEIVER_IP, packet_settings.FRAME_RECEIVER_PORT)
+    assert((ecss['type'] == 0) or (ecss['type'] == 1) == True )
+    assert((ecss['app_id'] < packet_settings.LAST_APP_ID) == True)
+    data_size = ecss['size']
+    packet_size = data_size + packet_settings.ECSS_DATA_HEADER_SIZE + packet_settings.ECSS_CRC_SIZE + packet_settings.ECSS_HEADER_SIZE
+    buf = bytearray(packet_size)
+    app_id = ecss['app_id']
+    app_id_ms = app_id & 0xFF00
+    app_id_ls = app_id & 0x00FF
+    buf[0] = ( packet_settings.ECSS_VER_NUMBER << 5 | ecss['type'] 
+               << 4 | packet_settings.ECSS_DATA_FIELD_HDR_FLG << 3 | app_id_ms);
+    buf[1] = app_id_ls
+    seq_flags = packet_settings.TC_TM_SEQ_SPACKET
+    seq_count = ecss['count']
+    seq_count_ms = seq_count & 0xFF00
+    seq_count_ls = seq_count & 0x00FF
+    buf[2] = (seq_flags << 6 | seq_count_ms)
+    buf[3] = seq_count_ls
+     
+    if ecss['type'] == 0 :
+        buf[6] = packet_settings.ECSS_PUS_VER << 4 ;
+    elif ecss['type'] == 1 :
+        buf[6] = ( packet_settings.ECSS_SEC_HDR_FIELD_FLG << 7 | packet_settings.ECSS_PUS_VER << 4 | ecss['ack']);    
+    buf[7] = ecss['ser_type']
+    buf[8] = ecss['ser_subtype']
+    buf[9] = ecss['dest_id']
+    
+    buf_pointer = packet_settings.ECSS_DATA_OFFSET
+    buf[buf_pointer:data_size] = ecss['data']
+    data_w_headers = data_size + packet_settings.ECSS_DATA_HEADER_SIZE + packet_settings.ECSS_CRC_SIZE -1
+    packet_size_ms = data_w_headers  & 0xFF00
+    packet_size_ls = data_w_headers  & 0x00FF
+    buf[4] = packet_size_ms
+    buf[5] = packet_size_ls
+    buf_pointer = buf_pointer + data_size
+    
+    for i in range(0,buf_pointer):
+        buf[buf_pointer + 1] = buf[buf_pointer + 1] ^ buf[i]
+    size = buf_pointer + 2
+    assert((size > packet_settings.MIN_PKT_SIZE and size < packet_settings.MAX_PKT_SIZE) == True)
+    sock.send_not_recv(buf)
+    return packet_settings.SATR_OK
+    
+def comms_off():
+    sock = Udpsocket(packet_settings.FRAME_RECEIVER_IP, packet_settings.FRAME_RECEIVER_PORT)
+    data = ctypes.create_string_buffer(25)
+    data[0:9] = 'RF SW CMD'
+    struct.pack_into("<I",data,9,0x593d55df)
+    struct.pack_into("<I",data,13,0x4d2f84c0)
+    struct.pack_into("<I",data,17,0x24d60191)
+    struct.pack_into("<I",data,21,0x9287b5fd)
+    d = bytearray(data)
+    sock.send(d)    
+    
+def comms_on():
+    sock = Udpsocket(packet_settings.FRAME_RECEIVER_IP, packet_settings.FRAME_RECEIVER_PORT)
+    data = ctypes.create_string_buffer(25)
+    data[0:9] = 'RF SW CMD'
+    struct.pack_into("<I",data,9,0xda4942a9)
+    struct.pack_into("<I",data,13,0xa7a45d61)
+    struct.pack_into("<I",data,17,0x413981b)
+    struct.pack_into("<I",data,21,0xa94ee2d3)
+    d = bytearray(data)
+    sock.send(d)    
--- satnogsclient/packet_settings.py
+++ satnogsclient/packet_settings.py
@@ -67,3 +67,60 @@ TC_ACK_EXE_PROG  =   0x04
 TC_ACK_EXE_COMP  =   0x08
 TC_ACK_ALL       =   0x0F
 
+FRAME_RECEIVER_IP = '127.0.0.1'
+FRAME_RECEIVER_PORT = 16886
+
+#SAT_RETURN_STATE
+
+SATR_PKT_ILLEGAL_APPID     = 0
+SATR_PKT_INV_LEN           = 1
+SATR_PKT_INC_CRC           = 2
+SATR_PKT_ILLEGAL_PKT_TP    = 3
+SATR_PKT_ILLEGAL_PKT_STP   = 4
+SATR_PKT_ILLEGAL_APP_DATA  = 5
+SATR_OK                    = 6
+SATR_ERROR                 = 7
+SATR_EOT                   = 8
+SATR_CRC_ERROR             = 9
+SATR_PKT_ILLEGAL_ACK       = 10
+SATR_ALREADY_SERVICING     = 11
+SATR_MS_MAX_FILES          = 12
+SATR_PKT_INIT              = 13
+SATR_INV_STORE_ID          = 14
+SATR_INV_DATA_LEN          = 15
+SATR_SCHEDULE_FULL         = 17 # Schedule array is full */
+SATR_SSCH_ID_INVALID       = 18 # Subschedule ID invalid */
+SATR_NMR_OF_TC_INVALID     = 19 # Number of telecommands invalid */
+SATR_INTRL_ID_INVALID      = 20 # Interlock ID invalid */
+SATR_ASS_INTRL_ID_INVALID  = 21 # Assess Interlock ID invalid */
+SATR_ASS_TYPE_ID_INVALID   = 22 # Assesment type id invalid*/        
+SATR_RLS_TIMET_ID_INVALID  = 23 # Relese time type ID invalid */
+SATR_DEST_APID_INVALID     = 24 # Destination APID in embedded TC is invalid */
+SATR_TIME_INVALID          = 25 # Release time of TC is invalid */
+SATR_TIME_SPEC_INVALID     = 26 #  Release time of TC is specified in a invalid representation*/
+SATR_INTRL_LOGIC_ERROR     = 27 # The release time of telecommand is in the execution window of its interlocking telecommand.*/
+SATR_SCHEDULE_DISABLED     = 28
+SATRF_OK                   = 29 # (0) Succeeded */
+SATRF_DISK_ERR             = 30 # (1) A hard error occurred in the low level disk I/O layer */
+SATRF_INT_ERR              = 31 # (2) Assertion failed */
+SATRF_NOT_READY            = 32 # (3) The physical drive cannot work */
+SATRF_NO_FILE              = 33 # (4) Could not find the file */
+SATRF_NO_PATH              = 34 # (5) Could not find the path */
+SATRF_INVALID_NAME         = 35 # (6) The path name format is invalid */
+SATRF_DENIED               = 36 # (7) Access denied due to prohibited access or directory full */
+SATRF_EXIST                = 37 # (8) Access denied due to prohibited access */
+SATRF_INVALID_OBJECT       = 38 # (9) The file/directory object is invalid */
+SATRF_WRITE_PROTECTED      = 39 # (10) The physical drive is write protected */
+SATRF_INVALID_DRIVE        = 40 # (11) The logical drive number is invalid */
+SATRF_NOT_ENABLED          = 41 # (12) The volume has no work area */
+SATRF_NO_FILESYSTEM        = 42 # (13) There is no valid FAT volume */
+SATRF_MKFS_ABORTED         = 43 # (14) The f_mkfs() aborted due to any parameter error */
+SATRF_TIMEOUT              = 44 # (15) Could not get a grant to access the volume within defined period */
+SATRF_LOCKED               = 45 # (16) The operation is rejected according to the file sharing policy */
+SATRF_NOT_ENOUGH_CORE      = 46 # (17) LFN working buffer could not be allocated */
+SATRF_TOO_MANY_OPEN_FILES  = 47 # (18) Number of open files > _FS_SHARE */
+SATRF_INVALID_PARAMETER    = 48 # (19) Given parameter is invalid */    
+SATRF_DIR_ERROR            = 49
+SATR_LAST                  = 50
+
+
