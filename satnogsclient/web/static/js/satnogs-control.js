d0628bd9556fadf4d950c9b2f653cfe9849745df
nchronas <nchronas@gmail.com>
2016-06-18 19:33:27 +0300

    added ui elements of time management

--- satnogsclient/web/static/js/satnogs-control.js
+++ satnogsclient/web/static/js/satnogs-control.js
@@ -13,6 +13,9 @@ $(function(){
 
      var elem= document.getElementById('service-param-test');
      elem.style.display= "none";
+
+     var elem= document.getElementById('service-param-time');
+     elem.style.display= "none";
    });
 
    $("#power-select").click(function() {
@@ -24,6 +27,9 @@ $(function(){
 
     var elem= document.getElementById('service-param-test');
      elem.style.display= "none";
+
+     var elem= document.getElementById('service-param-time');
+     elem.style.display= "none";
    });
 
    $("#test-select").click(function() {
@@ -34,6 +40,23 @@ $(function(){
      elem.style.display= "none";
 
      var elem= document.getElementById('service-param-test');
+     elem.style.display= "block";
+
+     var elem= document.getElementById('service-param-time');
+     elem.style.display= "none";
+   });
+
+   $("#time-select").click(function() {
+     var elem= document.getElementById('service-param-custom');
+     elem.style.display= "none";
+
+     var elem= document.getElementById('service-param-power');
+     elem.style.display= "none";
+
+     var elem= document.getElementById('service-param-test');
+     elem.style.display= "none";
+
+     var elem= document.getElementById('service-param-time');
      elem.style.display= "block";
    });
 
@@ -219,6 +242,16 @@ $(function(){
       var data = [fun_id, dev_id];
     }
     else if(selected_value == "Test") {
+      var app_id = $('#service-param-test-app_id').val();
+      var type = 1;
+      var ack = 0;
+
+      var service_type = 17;
+      var service_subtype = 1;
+      var dest_id = $('#service-param-test-dest_id').val();
+      var data = [];
+    }
+    else if(selected_value == "Time") {
       var app_id = $('#service-param-test-app_id').val();
       var type = 1;
       var ack = 0;
