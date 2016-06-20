47e530b139794a51ff30d7d72a5a33f5760d1ce5
George Vardakis <vardakis@csd.uoc.gr>
2016-06-20 17:42:15 +0300

    Finilizing RX function at the backend

--- satnogsclient/observer/gnuradio_handler.py
+++ satnogsclient/observer/gnuradio_handler.py
@@ -1,6 +1,7 @@
 import array
 import time
 import logging
+import json
 
 from satnogsclient import packet_settings
 from satnogsclient import settings as client_settings
@@ -10,17 +11,18 @@ from satnogsclient.observer import packet
 
 logger = logging.getLogger('satnogsclient')
 
-gnuradio_sock = Udpsocket(client_settings.GNURADIO_IP,client_settings.GNURADIO_UDP_PORT) #Gnuradio's udp listen port
-udp_local_sock = Udpsocket('127.0.0.1',client_settings.UDP_CLIENT_PORT) # Port in which client listens for frames from gnuradio
+udp_local_sock = Udpsocket(('127.0.0.1',client_settings.UDP_CLIENT_PORT)) # Port in which client listens for frames from gnuradio
+ecss_feeder_sock = Udpsocket([]) # The socket with which we communicate with the ecss feeder thread
 
 def write(buf):
-    gnuradio_sock.send(buf)
+    udp_local_sock.sendto(buf, (client_settings.GNURADIO_IP,client_settings.GNURADIO_UDP_PORT))
     
 def read_from_gnuradio():
     while True:
-        buf_in = udp_local_sock.listen()
+        conn = udp_local_sock.recv()
+        buf_in = conn[0]
         hldlc_buf = bytearray(0)
         hldlc.HLDLC_deframe(buf_in, hldlc_buf)
         ecss_dict = []
         packet.ecss_depacketizer(hldlc_buf,ecss_dict)
-    
+        ecss_feeder_sock.sendto(json.dumps(ecss_dict),('127.0.0.1',client_settings.ECSS_LISTENER_UDP_PORT))
