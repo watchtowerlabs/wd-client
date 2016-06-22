b57f218e95448b3d647d1bd310d552e477f58597
Kostis Triantafyllakis <ctriant@csd.uoc.gr>
2016-06-22 11:29:50 +0300

    Fetch ApplicationData from UI and add to ECSS

--- satnogsclient/web/app.py
+++ satnogsclient/web/app.py
@@ -104,7 +104,7 @@ def get_command():
                     'seq_count' : 59,
                     'ser_type' : int(requested_command['ecss_cmd']['PacketDataField']['DataFieldHeader']['ServiceType']),
                     'ser_subtype' : int(requested_command['ecss_cmd']['PacketDataField']['DataFieldHeader']['ServiceSubType']),
-                    'data' : bytearray(0),
+                    'data' : requested_command['ecss_cmd']['PacketDataField']['ApplicationData'],
                     'dest_id' : int(requested_command['ecss_cmd']['PacketDataField']['DataFieldHeader']['SourceID']),
                     'ack': int(requested_command['ecss_cmd']['PacketDataField']['DataFieldHeader']['Ack'])}
             print "CMD", ecss
