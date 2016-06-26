73924d2a1e04069857870b5474dbf0b7a7c0fc71
John Giannelos <johngiannelos@gmail.com>
2016-06-26 19:51:17 +0300

    Fix E222 pep8 issue.

--- satnogsclient/upsat/gnuradio_handler.py
+++ satnogsclient/upsat/gnuradio_handler.py
@@ -36,7 +36,7 @@ def read_from_gnuradio():
         ecss_dict = []
         ret = packet.deconstruct_packet(buf_in, ecss_dict, "gnuradio")
         ecss_dict = ret[0]
-        pickled =  cPickle.dumps(ecss_dict)
+        pickled = cPickle.dumps(ecss_dict)
         if ecss_dict['ser_type'] == packet_settings.TC_LARGE_DATA_SERVICE:
             ld_socket.sendto(pickled, ('127.0.0.1', client_settings.LD_UPLINK_LISTEN_PORT))
         else:
