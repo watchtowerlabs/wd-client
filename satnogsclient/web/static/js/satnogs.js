16ad771872443a6fec61cc151179159f33330d76
Kostis Triantafyllakis <ctriant@csd.uoc.gr>
2016-06-28 18:58:45 +0300

    Handle a mode switching custom command on back-end.

--- satnogsclient/web/static/js/satnogs.js
+++ satnogsclient/web/static/js/satnogs.js
@@ -5,10 +5,9 @@ init_view();
 $("#mode-switch li").click(function() {
       var selected = $(this).attr("data-value");
       Cookies.set('mode', selected);
-
       // Inform backend about the mode change
       request = encode_mode_switch(selected);
-      query_control_backend(request, 'POST', '/command', "application/json; charset=utf-8", "json", true);
+      query_backend(request, 'POST', '/command', "application/json; charset=utf-8", "json", true);
 
       $('#mode-switch :button').contents()[0].nodeValue = selected + " Mode ";
       document.cookie= selected;
@@ -43,7 +42,7 @@ $("#mode-switch li").click(function() {
      Cookies.set('mode', default_mode);
      // Send an initial request to backend in order to configure mode.
      request = encode_mode_switch(default_mode);
-     query_control_backend(request, 'POST', '/command', "application/json; charset=utf-8", "json", true);
+     query_backend(request, 'POST', '/command', "application/json; charset=utf-8", "json", true);
    }
   }
 
@@ -63,19 +62,14 @@ $("#mode-switch li").click(function() {
     return json_packet;
   }
 
-  function query_control_backend(data, post_mode, url, content_type, data_type, process_data) {
+  function query_backend(data, post_mode, url, content_type, data_type, process_data) {
     $.ajax({
         type: post_mode,
         url: url,
         contentType: content_type,
         dataType: data_type,
         data: data,
-        processData: process_data,
-        success: function(data) {
-            print_command_response(data);
-        },
-        error: function(data) {
-        }
+        processData: process_data
     });
   }
 
