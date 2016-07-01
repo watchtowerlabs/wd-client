6cf5640d1d55797c62b8be7dc507da0039e3ef7f
nchronas <nchronas@gmail.com>
2016-07-02 01:06:36 +0300

    fixed logic, and instead of or

--- satnogsclient/upsat/tx_handler.py
+++ satnogsclient/upsat/tx_handler.py
@@ -7,13 +7,16 @@ logger = logging.getLogger('satnogsclient')
 
 
 def send_to_backend(buf):
-    logger.debug('Send to backend called with backen %s', os.environ['BACKEND'])
+    logger.debug('Send to backend called with backend %s', os.environ['BACKEND'])
     curr_backend = os.environ['BACKEND']
-    if curr_backend != 'gnuradio' or curr_backend != 'serial':
+    print "what is backend? ", curr_backend
+    if curr_backend != 'gnuradio' and curr_backend != 'serial':
+        print "what is backend? ERROR ", curr_backend
         return 0
     if curr_backend == 'gnuradio':
         gnuradio_handler.write_to_gnuradio(buf)
         return 1
     if curr_backend == 'serial':
+        print "going to write in serial"
         serial_handler.write_to_serial(buf)
         return 1
