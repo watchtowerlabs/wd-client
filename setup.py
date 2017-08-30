c11bf27713eb62c1fc849317df4518dad9f1cc87
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2017-08-31 00:39:41 +0300

    Bump version to '0.4.1'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='satnogsclient',
-    version='0.4',
+    version='0.4.1',
     url='https://github.com/satnogs/satnogs-client/',
     author='SatNOGS project',
     author_email='dev@satnogs.org',
