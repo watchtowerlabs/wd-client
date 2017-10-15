dfe218cdd6ae32c8bb407026fb25b79cd90b5bbd
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2017-10-16 00:30:50 +0300

    Bump version to '0.5.1'
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='satnogsclient',
-    version='0.5',
+    version='0.5.1',
     url='https://gitlab.com/librespacefoundation/satnogs/satnogs-client',
     author='SatNOGS project',
     author_email='dev@satnogs.org',
