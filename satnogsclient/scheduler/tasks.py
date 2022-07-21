f81465c64a61c13a4d8b140b8227d2bdc1fdbda6
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2022-07-21 18:19:26 +0300

    Don't assign value to variable when used only once
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- satnogsclient/scheduler/tasks.py
+++ satnogsclient/scheduler/tasks.py
@@ -52,9 +52,8 @@ def spawn_observer(**kwargs):
     }
 
     LOGGER.debug('Observer args: %s', setup_kwargs)
-    setup = observer.setup(**setup_kwargs)
 
-    if setup:
+    if observer.setup(**setup_kwargs):
         LOGGER.info('Spawning observer worker.')
         timeout_timedelta = end - datetime.now(pytz.utc)
         if timeout_timedelta.total_seconds() == 0:
