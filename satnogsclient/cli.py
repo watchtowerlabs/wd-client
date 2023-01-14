dca4cdc3c1083e57a9ed430e4713edbdc2a23221
Fabian P. Schmidt <kerel@mailbox.org>
2023-01-14 04:10:31 +0100

    Add satnogs-create-waterfall CLI script
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

--- satnogsclient/cli.py
+++ satnogsclient/cli.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 
 from satnogsclient import config, settings
+from satnogsclient.artifacts import Artifacts
 from satnogsclient.waterfall import EmptyArrayError, Waterfall
 
 __version__ = config.VERSION
@@ -38,3 +39,40 @@ def main_satnogs_plot_waterfall():
         print('No waterfall data file found')
     except EmptyArrayError:
         print('Waterfall data array is empty')
+
+
+def main_satnogs_create_artifact():
+    """
+    CLI Script to create SatNOGS Artifact Files from SatNOGS Waterfall .dat files.
+    """
+    parser = argparse.ArgumentParser(
+        prog='SatNOGS Client - Create Artifacts',
+        description='Create a SatNOGS Artifact File from the given SatNOGS waterfall .dat File.',
+    )
+    parser.add_argument(
+        '--version',
+        action='version',
+        version=f'satnogs-client version {__version__}',
+    )
+    parser.add_argument(
+        'WATERFALL_DAT_FILE',
+        type=str,
+        help='The input waterfall dat file',
+    )
+    args = parser.parse_args()
+
+    output_file = Path('./') / (Path(args.WATERFALL_DAT_FILE).stem + '.h5')
+
+    try:
+        waterfall = Waterfall(args.WATERFALL_DAT_FILE)
+    except FileNotFoundError:
+        print('No waterfall data file found')
+    except EmptyArrayError:
+        print('Waterfall data array is empty')
+
+    metadata = {'note': 'Metadata is missing, file created by CLI tool.'}
+
+    artifact = Artifacts(waterfall, metadata)
+    artifact.create()
+    with open(output_file, 'wb') as f_out:
+        f_out.write(artifact.artifacts_file.read())
